3b6fc85bbb6481a5a0a0376cfbb7019f05f31e45
Scott Robbin <scott@robbin.co>
2018-05-20 07:55:47 -0500

    Rediscover template files on each build
    Fixes a bug where deleteing templates would cause a crash, or adding them
    would be ignored during a rebuild.

--- test/builder.test.js
+++ test/builder.test.js
@@ -1,11 +1,43 @@
+const fs = require('fs');
+const { resolve } = require('path');
+const tmp = require('tmp');
+
+const Builder = require('../lib/builder');
+const Utils = require('../lib/utils');
+
+const templatesDir = resolve(__dirname, 'fixtures', 'builder');
+
+describe('#tree', () => {
+  test('creates tree from multiple files', () => {
+    const builder = new Builder({ templatesDir });
+
+    expect(builder.tree).toMatchSnapshot();
+  });
+
+  test('picks up on changes to the template directory', () => {
+    const tmpDir = tmp.tmpNameSync();
+    Utils.copyFiles(templatesDir, tmpDir);
+
+    const builder = new Builder({ templatesDir: tmpDir });
+    expect(builder.tree).toMatchSnapshot();
+
+    const newHTML = `
+      {{#section offices}}
+        {{name}}
+        {{phone}}
+      {{/section}}
+    `;
+    fs.writeFileSync(resolve(tmpDir, 'new.html'), newHTML);
+    expect(builder.tree).toMatchSnapshot();
+
+    Utils.removeFiles(tmpDir);
+  });
+});
+
 describe('#build', () => {
   test.skip();
 });
 
 describe('#isDirty', () => {
-  test.skip();
-});
-
-describe('#tree', () => {
   test.skip();
 });
