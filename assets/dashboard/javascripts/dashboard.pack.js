fc08f78e28b3392e8702ad9bb16ff39c20023c7c
Scott Robbin <scott@robbin.co>
2018-06-12 09:07:09 -0500

    Include vendor CSS/JS in packed assets

--- assets/dashboard/javascripts/dashboard.pack.js
+++ assets/dashboard/javascripts/dashboard.pack.js
@@ -1,3 +1,13 @@
+const jQuery = require('jquery');
+window.$ = window.jQuery = jQuery;
+
+require('brace');
+
+// TODO: Include as packages
+require('../vendor/semantic-ui/semantic.min');
+require('../vendor/trix/trix');
+require('../vendor/jquery.tablesort');
+
 require('./dashboard/ace');
 require('./dashboard/hideMessage');
 require('./dashboard/range');
