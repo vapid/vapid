fba2e9881cc9b0eaa8038ef430915a66eb382096
Scott Robbin <scott@robbin.co>
2018-10-14 23:32:03 -0500

    WYSIWYG handler for shift+enter linebreaks

--- assets/dashboard/javascripts/dashboard/wysiwyg.js
+++ assets/dashboard/javascripts/dashboard/wysiwyg.js
@@ -1,5 +1,28 @@
 const Quill = require('quill');
 
+const Delta = Quill.import('delta');
+const Break = Quill.import('blots/break');
+const Embed = Quill.import('blots/embed');
+
+class Linebreak extends Break {
+  length () {
+    return 1;
+  }
+
+  value () {
+    return '\n';
+  }
+
+  insertInto(parent, ref) {
+    Embed.prototype.insertInto.call(this, parent, ref)
+  }
+}
+
+Linebreak.blotName = 'linebreak';
+Linebreak.tagName = 'BR';
+
+Quill.register(Linebreak)
+
 const options = {
   modules: {
     toolbar: [
@@ -10,7 +33,30 @@ const options = {
     ],
     clipboard: {
       matchVisual: false,
-    }
+    },
+    keyboard: {
+      bindings: {
+        linebreak: {
+          key: 13,
+          shiftKey: true,
+          handler: function (range) {
+            let currentLeaf = this.quill.getLeaf(range.index)[0]
+            let nextLeaf = this.quill.getLeaf(range.index + 1)[0]
+
+            this.quill.insertEmbed(range.index, 'linebreak', true, 'user');
+
+            // Insert a second break if:
+            // At the end of the editor, OR next leaf has a different parent (<p>)
+            if (nextLeaf === null || (currentLeaf.parent !== nextLeaf.parent)) {
+              this.quill.insertEmbed(range.index, 'linebreak', true, 'user');
+            }
+
+            // Now that we've inserted a line break, move the cursor forward
+            this.quill.setSelection(range.index + 1, Quill.sources.SILENT);
+          }
+        },
+      },
+    },
   },
   theme: 'snow',
 };
