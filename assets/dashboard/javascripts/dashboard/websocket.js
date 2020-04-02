d621d51d2a680686a772f24d1bf136b0ca8d092b
Johannes Jakob <JohJakob@users.noreply.github.com>
2020-04-02 23:35:13 +0200

    fix: Make Websocket Connection Secure (#178)
    Make Websocket Connection Secure when HTTPS is used

--- assets/dashboard/javascripts/dashboard/websocket.js
+++ assets/dashboard/javascripts/dashboard/websocket.js
@@ -1,7 +1,7 @@
 const liveReload = document.querySelector('meta[name=livereload]');
 
 if ('WebSocket' in window && liveReload) {
-  const ws = new WebSocket(`ws://${location.host}/livereload`);
+  const ws = new WebSocket(`ws${location.protocol == 'https:' ? 's' : ''}://${location.host}/livereload`);
 
   document.addEventListener("turbolinks:load", () => {
     const $build = $('.build.button');
